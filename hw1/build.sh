0a7a7125f84392f561acb07702fc8f1b0de593e5
Oleksandra2020 <oleksandrahutor@gmail.com>
2022-03-30 09:39:42 +0300

    Added hw1

---
+++ hw1/build.sh
@@ -0,0 +1,1 @@
+docker build . -t hutor_hw1_image:1.0
---
+++ hw1/hutor_hw1.py
@@ -0,0 +1,9 @@
+import datetime 
+
+def main():
+	print(datetime.datetime.now())
+	print("Oleksandra Hutor")
+
+
+if __name__ == "__main__":
+	main()
